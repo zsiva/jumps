219b2c923d9895997573f4e9032b41bb69c71dbe
zsiva <m.carrizo.ibarra@gmail.com>
2016-02-03 22:27:24 +0100

    add ranking

---
+++ frontend/components/ranking/index.js
@@ -0,0 +1,10 @@
+import _ from 'lodash';
+import STATES from '../../../constants/states';
+
+module.exports = angular.module('jumps.RankingController', [])
+    .controller('RankingController', RankingController);
+
+function RankingController(authorData, authorService, $state) {
+    const vm = this;
+
+}
