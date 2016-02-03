cea760d579e7d733ee1b720e65392a8e8f3bd56d
zsiva <m.carrizo.ibarra@gmail.com>
2016-02-03 22:56:35 +0100

    delete blog

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -20,26 +20,14 @@ app.config(['$routeProvider', function ($routeProvider) {
     .when("/visited-countries", {templateUrl: "components/countries/template.html", controller: "PageCtrl"})
     .when("/services", {templateUrl: "partials/services.html", controller: "PageCtrl"})
     .when("/contact", {templateUrl: "partials/contact.html", controller: "PageCtrl"})
-    // Blog
-    .when("/blog", {templateUrl: "partials/blog.html", controller: "BlogCtrl"})
-    .when("/blog/post", {templateUrl: "partials/blog_item.html", controller: "BlogCtrl"})
     // else 404
     .otherwise("/404", {templateUrl: "partials/404.html", controller: "PageCtrl"});
 }]);
 
 /**
- * Controls the Blog
- */
-app.controller('BlogCtrl', function (/* $scope, $location, $http */) {
-  console.log("Blog Controller reporting for duty.");
-});
-
-/**
  * Controls all other Pages
  */
 app.controller('PageCtrl', function (/* $scope, $location, $http */) {
-  console.log("Page Controller reporting for duty.");
-
   // Activates the Carousel
   $('.carousel').carousel({
     interval: 5000
