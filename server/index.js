01865e6542c19ea0e8076eceef23c6a9a73bd1bd
zsiva <m.carrizo.ibarra@gmail.com>
2016-02-03 22:10:11 +0100

    change tpl structure

--- server/index.js
+++ server/index.js
@@ -11,15 +11,15 @@ app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 
-app.use('/frontend', express.static(path.join(__dirname, '../frontend/')));
-app.use(express.static('../frontend'));
+app.use('/', express.static(path.join(__dirname, '../frontend/')));
+//app.use(express.static('../frontend'));
 app.use('/css',express.static(path.join(__dirname, '../frontend/css')));
 
 //app.use('/', routes);
 
-app.get('/*', function (req, res) {
-    console.log(baseHtml)
-    res.sendFile(baseHtml);
-});
+// app.get('/*', function (req, res) {
+//     console.log(baseHtml)
+//     res.sendFile(baseHtml);
+// });
 
 app.listen(3000);
