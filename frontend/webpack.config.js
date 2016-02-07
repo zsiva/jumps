84b10e7c5f9529f332ac21a9834b23e709e52234
zsiva <m.carrizo.ibarra@gmail.com>
2016-02-07 21:19:02 +0100

    update webpack config

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,44 +1,41 @@
-module.exports = {
-    entry: {
-        'app': './src/js/index.js'
-    },
-    output: {
-        'filename': './build/[name].js'
-    },
-    resolve: {
-        root: __dirname + '/src',
-        extensions: ['', '.js', 'jpg', '.json']
-    },
-    module: {
-        preLoaders: [
-            {
-                test: /\.(js)$/,
-                exclude: /node_modules/,
-                loader: 'jshint-loader'
+var config = {
+  entry: {
+      'app': './src/js/index.js'
+  },
+  output: {
+      path: './build',
+      filename: '[name].js'
+  },
+  resolve: { alias: {} },
 
-            }
-        ],
-        loaders: [
-            {
-                test: /\.js?$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: 'babel-loader',
-                query: {
-                    cacheDirectory: true,
-                    presets: ['es2015']
-                }
-            },
-            {
-                test: /\.html$/,
-                loader: 'html'
-            },
-            {
-                test: /\.(jpe?g|png|gif|svg|json)$/i,
-                loaders: [
-                    'file?hash=sha512&digest=hex&name=./build/assets/[name].[ext]',
-                    'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
-                ]
-            }
-        ]
-    }
-}
+  module: {
+      preLoaders: [
+          {
+              test: /\.(js)$/,
+              exclude: /node_modules/,
+              loader: 'jshint-loader'
+
+          }
+      ],
+      loaders: [
+          {
+              test: /\.js?$/,
+              exclude: /(node_modules|bower_components)/,
+              loaders: ['ng-annotate', 'babel?presets[]=es2015']
+          },
+          {
+              test: /\.html$/,
+              loader: 'html'
+          },
+          {
+              test: /\.(jpe?g|png|gif|svg|json)$/i,
+              loaders: [
+                  'file?hash=sha512&digest=hex&name=./assets/[name].[ext]',
+                  'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+              ]
+          }
+      ]
+  }
+};
+
+module.exports = config;
