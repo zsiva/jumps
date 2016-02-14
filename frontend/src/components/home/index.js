f89e09bfa74739f9de19cfd081d47f6d0c73fa0d
zsiva <m.carrizo.ibarra@gmail.com>
2016-02-14 18:19:48 +0100

    updatae home layout

--- frontend/src/components/home/index.js
+++ frontend/src/components/home/index.js
@@ -1,5 +1,6 @@
 import SLIDER from '../../data/slides';
 import GALLERY from '../../data/gallery';
+import STATES from '../../constants/states';
 
 module.exports = angular.module('jumps.homeController', [])
   .controller('homeController', homeController);
@@ -9,4 +10,5 @@ function homeController() {
 
   vm.slides = SLIDER.SLIDES;
   vm.galleryRows = _.chunk(GALLERY.ITEMS, GALLERY.items_row_home);
+  vm.galleryUrl = STATES.GALLERY;
 }
