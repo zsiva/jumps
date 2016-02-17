3d949f6d244b80a356287a2ae8b960d4f0eebc59
zsiva <m.carrizo.ibarra@gmail.com>
2016-02-17 12:19:59 +0100

    update countries and ranking

--- frontend/src/components/countries/index.js
+++ frontend/src/components/countries/index.js
@@ -1,8 +1,3 @@
-import STATES from '../../constants/states';
-require('vmap');
-require('world_map');
-require('custom_map');
-
 module.exports = angular.module('jumps.countriesController', [])
     .controller('countriesController', countriesController);
 
