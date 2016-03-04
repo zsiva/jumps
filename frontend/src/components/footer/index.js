0186b264b2099c0f09b5e0d5c623f6db09d5247b
zsiva <m.carrizo.ibarra@gmail.com>
2016-03-04 17:55:46 +0100

    remove ctrl from footer

--- frontend/src/components/footer/index.js
+++ frontend/src/components/footer/index.js
@@ -3,9 +3,6 @@ module.exports = angular.module('jumps.footer', []).directive('footer', footer);
 function footer() {
     return {
         restrict: 'E',
-        bindToController: true,
-        controllerAs: 'vm',
-        scope: {},
         template: require('./template.html')
     };
 }
