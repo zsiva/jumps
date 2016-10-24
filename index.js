7662b4471ddfcad00e6189f48afd6ce0855d17b4
Marta Carrizo <m.carrizo.ibarra@gmail.com>
2016-10-24 19:41:59 +0200

    update express config

--- index.js
+++ index.js
@@ -2,17 +2,9 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-var routes = require('./routes');
-
 var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
-
-
-// app.use('/', express.static(path.join(__dirname, '../frontend/')));
-// //app.use(express.static('../frontend'));
-// app.use('/css',express.static(path.join(__dirname, '../frontend/css')));
-var baseHtml = path.join(__dirname, './frontend/index.html');
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
@@ -23,10 +15,8 @@ app.use('/build', express.static(path.join(__dirname, '/frontend/build')));
 
 app.use('/src/css',express.static(path.join(__dirname, '/frontend/src/css')));
 
-//app.use('/', routes);
-
-app.get('/*', function (req, res) {
-    res.sendFile(baseHtml);
+app.get('/*', function(req, res) {
+  res.sendFile('frontend/index.html' , { root : __dirname});
 });
 
 app.listen(3000);
